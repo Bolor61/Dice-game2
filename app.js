2f4121467387d391e06106ef4943389a7250131a
bolor <bolor61@gmail.com>
2021-01-18 23:39:42 -0600

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -18,20 +18,29 @@ var roundScore = 0 ;
 
 // Shoonii ali talaaraa buusniig hadgalah % sagch heregtei 1-6 gesen utgiig ene % sagchid sanamsarguigeer uusgej ugnu.
 
-var dice = Math.floor (Math.random()*6)+ 1;
+var diceNumber = Math.floor (Math.random()*6)+ 1;
 
-// <div class="player-score" id="score-0">43</div>
+
 
 // window.document.querySelector('#score-0').textContent = dice;
-// document.querySelector('#score-1').innerHTML = "<em> Yes! </em>";
+
 
 // programm ehlehed beltgii 
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
+document.getElementById('score-0').textContent='0';
 
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
+document.getElementById('score-1').textContent = '0';
 
- document.querySelector('.dice').style.display = 'none'
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
-console.log(' shoo '+ dice) 
+
+var diceDom = document.querySelector('.dice');
+diceDom.style.display = 'none'
+
+ document.querySelector('.btn-roll').addEventListener('click', function () { 
+    var diceNumber = Math.floor (Math.random()*6)+ 1;
+    diceDom .style.display = 'block';
+    diceDom .src = 'dice-' + diceNumber + '.png';    
+    
+ }
+)
