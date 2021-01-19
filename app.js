acb9e9cdb50c018c407392362f8b9cfad0362665
bolor <bolor61@gmail.com>
2021-01-19 13:06:39 -0600

    onoog oruulah hold tovchiig darhad hadgalah, toglogchiin  eelj solih

--- app.js
+++ app.js
@@ -1,35 +1,21 @@
 // toglogchiin eeljiig hadgalagch % sagch 1-r toglogchiig o, 2- r toglogchiig 1 geh temdeglii
 var activePlayer = 0;
 
-
-
-
 // toglogchdiin tsugluulsan onoog hadgalah % sagch 
 var scores = [0, 0];
 
-
-
-
-
 // toglogchiin eeljin deer tsugluulj bgaa onoog hadgalagch % sagch 
 var roundScore = 0 ;
-
-
-
 // Shoonii ali talaaraa buusniig hadgalah % sagch heregtei 1-6 gesen utgiig ene % sagchid sanamsarguigeer uusgej ugnu.
 
 var diceNumber = Math.floor (Math.random()*6)+ 1;
-
-
 
 // window.document.querySelector('#score-0').textContent = dice;
 
 
 // programm ehlehed beltgii 
 document.getElementById('score-0').textContent='0';
-
 document.getElementById('score-1').textContent = '0';
-
 document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
@@ -51,7 +37,6 @@ diceDom.style.display = 'none'
         roundScore = roundScore + diceNumber;
         document.getElementById('current-' + activePlayer).textContent = roundScore;
 
-
     } else {
         // 1 buusan tul toglogchiin eeljiig ene hesegt solij ugnu.
 
@@ -67,26 +52,60 @@ diceDom.style.display = 'none'
         // herev idevhtei toglogch 0 baival idevhitei toglochiig 1 bolgo
         // ugui bol idevhitei toglogchiig 0 bolgo
         activePlayer === 0 ? (activePlayer = 1) :(activePlayer = 0)
+         /* deerh tei yag adilhan 
+        if (activePlayer === 0){
+            activePlayer = 1;
+        }else{
+            activePlayer= 0
+        }*/
 
         // ulaan tsagiig shiljuuleh codiig hiie
         document.querySelector('.player-0-panel').classList.toggle('active')
         document.querySelector('.player-1-panel').classList.toggle('active')
 
-
         // shoog tur alga bolgoh
 
-        diceDom.style.display ='none'
-
-        /* deerh tei yag adilhan 
-        if (activePlayer === 0){
-            activePlayer = 1;
-
-        }else{
-            activePlayer= 0
-        }*/
+        diceDom.style.display ='none'       
        
     }
-    
-    
+        
  }
 )
+
+// Hold tovchnii event listener
+document.querySelector('.btn-hold').addEventListener("click", function(){
+    // ug toglogchiin tsugluulsan eeljnii onoog global onoon deer n nemj ugnu.
+    // var scores = [0, 0];
+    // if (activePlayer === 0){
+    //     scores[0] = scores[0] + roundScore;
+    // }else {
+    //     scores[0] = scores[1] + roundScore;
+    // }
+    scores[activePlayer] = scores[activePlayer] +roundScore
+    // delgets deer onoog n uurchilnu
+    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer]
+    // eeljiin onoog n 0 bolgono 
+    roundScore = 0;
+    document.getElementById('current-' + activePlayer).textContent = '0';
+
+
+
+    // toglogchiin eeljiig solnino.
+    // 1 buusan tul toglogchiin eeljiig ene hesegt solij ugnu.
+        //ene toglogchiin eeljindee tsugluulsan onoog 0 bolgo
+        roundScore = 0
+        document.getElementById('current-' + activePlayer).textContent = 0;
+
+        // toglogchiin eeljiig nuguu toglogch ruu shiljuulne
+
+        // herev idevhtei toglogch 0 baival idevhitei toglochiig 1 bolgo
+        // ugui bol idevhitei toglogchiig 0 bolgo
+        activePlayer === 0 ? (activePlayer = 1) :(activePlayer = 0)        
+        // ulaan tsagiig shiljuuleh codiig hiie
+        document.querySelector('.player-0-panel').classList.toggle('active')
+        document.querySelector('.player-1-panel').classList.toggle('active')
+
+        // shoog tur alga bolgoh
+        diceDom.style.display ='none'    
+
+})
